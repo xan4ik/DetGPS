b5bbc6480d0a3143e7e8e923d2483e923252c442
unknown <xanitus2000@mail.ru>
2021-02-12 19:44:23 +0300

    Fix manipulator

--- NetGPS/NMEA_GPS/GPS/ManipulatorGPS.cs
+++ NetGPS/NMEA_GPS/GPS/ManipulatorGPS.cs
@@ -5,43 +5,50 @@ namespace GPS
 { 
     public class ManipulatorGPS
     {
-        private static double earthRadius;
+        private const double EarthRadius = 6378137.0;
+        private const double RADIANS = Math.PI / 180;
+        private const double DEGREES = 180 / Math.PI;
 
-        static ManipulatorGPS()
+        public GPS<double> MoveGPS(GPS<double> from, double distance, double brearing)
         {
-            earthRadius = 6378137;
+            double lat1 = from.Latitude * RADIANS;
+            double lon1 = from.Longitude * RADIANS;
+            double radbear = brearing * RADIANS;
+
+            double lat2 = Math.Asin(Math.Sin(lat1) * Math.Cos(distance / EarthRadius) + Math.Cos(lat1) * Math.Sin(distance / EarthRadius) * Math.Cos(radbear));
+            double lon2 = lon1 + Math.Atan2(Math.Sin(radbear) * Math.Sin(distance / EarthRadius) * Math.Cos(lat1), Math.Cos(distance / EarthRadius) - Math.Sin(lat1) * Math.Sin(lat2));
+
+            return new GPS<double>(lat2 * DEGREES, lon2 * DEGREES);
         }
 
-        public GPS<float> RecountGPS(float latitude, float longitude, float directionAngle, float offsetX, float offsetY)
+        public GPS<double> RecountGPS(double latitude, double longitude, double rangeX, double rangeY, double bearing)
         {
-            var degreeAngle = ConvertAngleFromCompass(directionAngle);
-            var radianAngle = ConvertDegreeToRadian(degreeAngle);
-            var _latitude = latitude + (180 / Math.PI) * (offsetY / earthRadius) * Math.Sin(radianAngle);
-            var _longitude = longitude + (180 / Math.PI) * (offsetY / earthRadius) / Math.Cos(latitude) * Math.Cos(radianAngle);
+            var angle = ConvertAngleFromCompass(bearing) * RADIANS;
+            var _X = rangeX * Math.Cos(angle) - rangeY * Math.Sin(angle);
+            var _Y = rangeX * Math.Sin(angle) + rangeY * Math.Cos(angle);
 
-            return new GPS<float>((float)_latitude, (float)_longitude);
-        }
+            var new_latitude = latitude + (_Y / EarthRadius) * DEGREES; // x
+            var new_longitude = longitude + (_X / EarthRadius) * DEGREES / Math.Cos(latitude * RADIANS); //y
 
-        public GPS<double> RecountGPS(double latitude, double longitude, double directionAngle, double offsetX, double offsetY)
-        {
-
-            var degreeAngle = ConvertAngleFromCompass(directionAngle);
-            var radianAngle = ConvertDegreeToRadian(degreeAngle);
-            var _latitude = latitude + (180 / Math.PI) * (offsetY / earthRadius) * Math.Sin(radianAngle);
-            var _longitude = longitude + (180 / Math.PI) * (offsetY / earthRadius) / Math.Cos(latitude) * Math.Cos(radianAngle);
-            
-            return new GPS<double>(_latitude, _longitude);
-        }
-
-        private double ConvertDegreeToRadian(double degreeAngle)
-        {
-            return degreeAngle * (Math.PI / 180);
+            return new GPS<double>(new_latitude, new_longitude);
         }
 
         private double ConvertAngleFromCompass(double directionAngle)
         {
             return 360 - (directionAngle - 90);
-        }     
+        }
+
+        private static double RotateX(double x, double y, double angle)
+        {
+            angle *= RADIANS;
+            return x * Math.Cos(angle) - y * Math.Sin(angle); 
+        }
+
+        private static double RotateY(double x, double y, double angle)
+        {
+            angle *= RADIANS;
+            return x * Math.Sin(angle) + y * Math.Cos(angle);
+        }
     }
 }
 
